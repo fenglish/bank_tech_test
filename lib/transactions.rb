7507cb3ea447cfac9b354c0ac22d1c7ea471494e
Asuka Ochi <asukaochi@gmail.com>
2017-01-25 14:06:45 +0000

    add store_data_to_transaction method

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,7 +1,20 @@
+# This class should create single transactions and store them
+
 class Transactions
 
+  attr_reader :all, :current_transaction
+
+  def initialize( transaction_klass )
+    @current_transaction = transaction_klass
+    @all = []
+  end
+
   def create_transaction
-    Transaction.new
+    @current_transaction = @current_transaction.new
+  end
+
+  def store_data_to_transaction( data )
+    @current_transaction.store( data )
   end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,10 +2,18 @@ require "transactions"
 
 describe Transactions do
 
-  subject( :transactions ){ described_class.new }
+  subject( :transactions ){ described_class.new( transaction ) }
+  let( :transaction ){ double :transaction_klass }
 
   it "should create a instance of Transaction class" do
-    expect( transactions.create_transaction ).to be_an_instance_of( Transaction )
+    allow( transaction ).to receive( :new ).and_return( transaction )
+    expect( transactions.create_transaction ).to eq( transactions.current_transaction )
+  end
+
+  it "should store passed data to an instance of Transaction class" do
+    data = { date: "01/01/2017", credit: 50, debit: "", balance: 50 }
+    allow( transaction ).to receive( :store ).and_return( data )
+    expect( transactions.store_data_to_transaction( data ) ).to eq( data )
   end
 
 end
